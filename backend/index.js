5a07081c78b4ac828fdffa095786575da54e17e5
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-12-21 23:58:01 -0600

    Arreglando el puerto del servidor Ahora sera el 4000, iniciamos con seguridad con dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env'
+})
+
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log("Servidor corriendo en el puerto" + 400)
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Servidor corriendo en el puerto: '${process.env.SERVER_PORT}'`)
 })
