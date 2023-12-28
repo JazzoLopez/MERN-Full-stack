8e65f251b7c2cdc8b93b6f81ecebbdaa4f043b68
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-12-27 19:52:02 -0600

    Generando el token para el usuario

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,25 @@
 import User from "../models/users.js"
+import generateId from "../helpers/generateID.js"
 
 const users = (req, res) => {
     res.json({msg:'Desde api/usuarios'})
 }
 
 const register = async (req, res) => {
+    const {email} = req.body;
+    const userExist = await User.findOne({email})
+
+    if (userExist){
+        const error = new Error('Usuario ya registrado')
+        return res.status(400).json({msg: error.message})
+     }
+
     try{
         const newUser = new User(req.body);
-        const {email} = req.body;
-        const userExist = await User.findOne({email})
-        
-        if (userExist){
-           const error = new Error('Usuario ya registrado')
-           return res.status(400).json({msg: error.message})
-        }
-        else{
+        newUser.token = generateId();
         const userSave = await newUser.save();
         console.log(req.body)
         res.json({userSave})
-    }
     }catch(err){
         console.log(err.msg)
     }
